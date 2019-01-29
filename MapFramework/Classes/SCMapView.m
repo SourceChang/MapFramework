7ea6a8f3ab1e380926dceaa02d4650d77711e21c
Source <source_chang@SourcedeiMac.local>
2019-01-29 18:55:46 +0800

    【Feature】添加百度地图 SDK

---
+++ MapFramework/Classes/SCMapView.m
@@ -0,0 +1,20 @@
+//
+//  SCMapView.m
+//  MapFramework
+//
+//  Created by Source on 2019/1/29.
+//
+
+#import "SCMapView.h"
+
+@implementation SCMapView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
